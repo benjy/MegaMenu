7c255465d0d87e57b994a7c8e313dccf1aba96e0
Ben <ben@me.com>
2013-01-06 09:43:09 +0800

    updated email

--- app/code/local/DevBlog/MegaMenu/Block/ThreeColumns.php
+++ app/code/local/DevBlog/MegaMenu/Block/ThreeColumns.php
@@ -4,7 +4,7 @@
      *
      *
      * @package    DevBlog_MegaMenu
-     * @author     Ben (ben@dhmedia.com.au)
+     * @author     Ben (ben@devblog.com.au)
      */
 
 class DevBlog_MegaMenu_Block_ThreeColumns extends DevBlog_MegaMenu_Block_Menu
--- app/code/local/DevBlog/MegaMenu/Helper/Data.php
+++ app/code/local/DevBlog/MegaMenu/Helper/Data.php
@@ -2,7 +2,7 @@
 
 /**
  * @package DevBlog_MegaMenu
- * @author Ben Dougherty <ben@dhmedia.com.au>
+ * @author Ben Dougherty <ben@devblog.com.au>
  */
 class DevBlog_MegaMenu_Helper_Data extends Mage_Core_Helper_Data
 {
